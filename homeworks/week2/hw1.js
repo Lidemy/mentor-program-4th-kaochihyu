c1e1b44c0b3f036d666dc4526a249ce0ae5a596e
Ckao <k2307874@gmail.com>
2020-06-30 21:08:01 +0800

    week2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,10 @@
 function printStars(n) {
-  
+	if (n >= 1 && n <= 30) {
+		for (var i = 1; i <= n; i++) {
+			var stars = ''
+			console.log(stars += '*')
+	  }
+	}  
 }
 
 printStars(5);
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,12 @@
 function capitalize(str) {
-  
+	var arr = str.split('')
+	if (str.charCodeAt(0) >= 97 && str.charCodeAt(0) <= 122) {
+		var change = arr[0].toUpperCase()
+		str = str.replace(arr[0], change)
+		return str
+	} else {
+		return str
+	}
 }
 
 console.log(capitalize('hello'));
