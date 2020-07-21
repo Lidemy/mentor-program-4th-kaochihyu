e21e942fdc062821c8d165ecc00214ba4dd05857
Ckao <k2307874@gmail.com>
2020-07-22 00:45:25 +0800

    week4 hw1

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,11 +1,16 @@
 const request = require('request');
 
 const baseUrl = 'https://lidemy-book-store.herokuapp.com';
+
 request(`${baseUrl}/books?_limit=10`,
-  (error, response, body) => {
-    const json = JSON.parse(body);
-    for (let i = 0; i <= 9; i += 1) {
-      const item = json[i];
-      console.log(`${item.id} ${item.name}`);
+  (err, response, body) => {
+    let json = '';
+    try {
+      json = JSON.parse(body);
+    } catch (error) {
+      console.log(err);
+    }
+    for (let i = 0; i < json.length; i += 1) {
+      console.log(`${json[i].id} ${json[i].name}`);
     }
   });
